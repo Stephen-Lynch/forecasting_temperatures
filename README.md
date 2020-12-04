44cff9fbee29efb0f9c7b60f1675a6f007043247
stephen-lynch <stephen.james.lynch@gmail.com>
2020-12-03 20:23:37 -0700

    Fixing up ReadMe

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # forecasting_temperatures
 
-With temperatures slowly on the rise and global warming on the forefront of every nations mind, I wanted to take a shot at making some visualizations to show just how much temperatures have risen in the past 10 years as well as predict what the temperatures will look like in the years to come for some of the worlds hottest cities.
-![image](images/Figure_1.png)
+
+ With temperatures slowly on the rise and global warming on the forefront of every nations mind, I wanted to take a shot at making some visualizations to show just how much temperatures have risen in the past 10 years as well as predict what the temperatures may look like in the years to come for some of the worlds hottest cities.
+
+ I started out with a dataset from the United Nations which had the average temperature of any given day for cities all over the world ranging from the years 1995 to 2020. For the purposes of this project I focused on the years between 2005 and 2016 since many of the nations included in this didn't update their daily temperatures until around the year 2000.  
+
+ DATA HERE
+
+ I wanted to start out with something simple and just show a basic graph of global yearly average temperatures.
+
+ yearly graph temps
+
+ It's hard to tell just by this, if there is any sort of general trend going on with our data or if there are just mostly seemingly random spikes in years. Also, I was honestly expecting this to show a much larger increase in temperature over an 11 year period even if it's based globally which would generally have smaller temperature increase. Anyways let's take a look at a seasonal decomposition of the averages globally by month to get a better grasp of what's going on.
+ 
+ 
+![image](images/Figure_1.png) 
+
+ As you can see, albiet it seems to be a very small increase, there is a general trend in temperatures going up between the years 2005 and 2016. There also seems to be a seasonality trend, which comes as no surprise to anyone, however it is something I'm going to have to account for in the SARIMAX model.
+
+ Going forward, I wanted to figure out the 5 hottest cities based on their average yearly temperature as I believed these cities would be the best to model and show just how hot they're getting.
+ 
+ GRAPH HERE
+ 
+ One thing I found interesting about these cities is that their average yearly temperature hasn't gone up by much. There are certain spikes to be certain and the majority of them seem to be on some sort of upwards trend since 2008, however they're relatively flat lined at least compared to how I originally thought in terms of temperature increases. One thing to note here however is that every single one of these cities has a yearly average of over 80 degrees. Being from Colorado, it's hard to imagine living in a place that has weather that's so hot that it's yearly average temperature is something I would consider toasty here. Many of these places get much hotter than this, such as Niamey, Nigeria hitting a max just this year of 120 degrees Fahrenheit
+
+ With all of this in mind, I wanted to move on and start working on my model. After doing some research on which machine learning model would perform best, I ended up deciding to apply a SARIMAX model to each of the cities, as it seemed to handle seasonality and general trends extremely well.
+
+ train vs test Graphs
+
+ Unfortunately my model seems to be overfitting to each of the cities I picked and I wasn't 
+
+ data vs holdout data
+
+ But that Finally that brings us to forecasting, which I believe shows the problem my model ends up having
+
+ forecasting graphs
+
+ It looks like it failed to pick up on any general trend of rising temperatures which is what I was afraid of happening based on the yearly averages. Temperature incrases and decreases are just too small, at least based on a month by month average over the years for my model to be able to pick anything up.
+ 
+ Conclusions:
+ My goal orginally with this project was to try and show how much temperatures are rising by showing just how hot the world's hottest cities are getting. It turns out I severely understimated just how slowly the average monthly temperatures are rising. This made it hard for my model to see a general trend happening at all, which makes it not a great predictor for the far future.
+
+ Future Steps:
+ As any data scientist would say, I would love to get my hands on more data to see if my model would then be able to predict the general trend any easier. Also fixing the overfitting on this model would seem Also instead of basing my predictions off of monthly values, perhaps I should have tried to go with weekly, or even daily to try and forecast better. Finally I believe working with different cities may have yielded better results in terms of picking up a trend, as many of these cities haven't seen a massive degree in difference between their average yearly in 2005 and average yearly in 2016.
--- src/eda.py
+++ src/eda.py
@@ -11,6 +11,7 @@ from pmdarima import auto_arima
 import warnings 
 warnings.filterwarnings("ignore") 
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tools.eval_measures import rmse
 
 def df_create(df, drop, groupby, reset_index = True):
     '''
@@ -61,86 +62,94 @@ def drop_month_year(df):
 city_temp = pd.read_csv('data/cleaned_city_temps.csv')
 city_temp = city_temp.drop('Unnamed: 0', axis = 1)
 city_temp = city_temp.loc[city_temp['Year'] > 2004]
+city_temp_train = city_temp.loc[city_temp['Year'] < 2017]
+city_temp_ho = city_temp.loc[(city_temp['Year'] > 2016) * (city_temp['Year'] < 2019)]
 
 
 #Created global_temp as a basic EDA for some cool graphs
 #As well as do a Seasonal Decomposition for it
-global_temp = df_create(city_temp, ['Region', 'Country', 'City', 'Day'], 
+global_temp = df_create(city_temp_train, ['Region', 'Country', 'City', 'Day'], 
                         ['Year', 'Month'])
+global_temp_year = df_create(city_temp_train, ['Region', 'Country', 'City', 'Day', 'Month'], ['Year'], reset_index = False)
 global_temp['Date'] = datetime_month(global_temp)
 global_temp = drop_month_year(global_temp)
-seasonal_result = seasonal_decompose(global_temp, model='additive')
+# seasonal_result = seasonal_decompose(global_temp, model='additive')
 
 
 
-city_highest = df_create(city_temp, ['Region',], 
+city_highest = df_create(city_temp_train, ['Region',], 
                                     ['Country', 'City', 'Year', 'Month', 'Day'])
-city_highest = city_highest.loc[city_highest['Year'] == 2019 ]
+city_highest = city_highest.drop(['Day', 'Month'], axis = 1).groupby(['Country', 'City', 'Year']).mean().reset_index()
+city_highest = city_highest.loc[city_highest['Year'] == 2016].sort_values('AvgTemperature', ascending=False)
+
 
                                 
 
 # Creating the 5 cities I'm going to model
-niamey = city_temp.loc[city_temp['City'] == 'Niamey']
-kuwait = city_temp.loc[city_temp['City'] == 'Kuwait']
-dubai = city_temp.loc[city_temp['City'] == 'Dubai']
-doha = city_temp.loc[city_temp['City'] == 'Doha']
-chennai = city_temp.loc[city_temp['City'] == 'Chennai (Madras)']
+niamey = city_temp_train.loc[city_temp_train['City'] == 'Niamey']
+niamey_year_ho = city_temp_ho.loc[city_temp_ho['City'] == 'Niamey']
+kuwait = city_temp_train.loc[city_temp_train['City'] == 'Kuwait']
+dubai = city_temp_train.loc[city_temp_train['City'] == 'Dubai']
+doha = city_temp_train.loc[city_temp_train['City'] == 'Doha']
+chennai = city_temp_train.loc[city_temp_train['City'] == 'Chennai (Madras)']
 
 niamey = df_create(niamey, ['Region', 'Country', 'City', 'Day'], ['Year', 'Month'])
+niamey_year = niamey.drop('Month', axis = 1).groupby('Year').mean('AvgTemperature')
 niamey['Date'] = datetime_month(niamey)
 niamey = drop_month_year(niamey)
 
 kuwait = df_create(kuwait, ['Region', 'Country', 'City', 'Day'], ['Year', 'Month'])
+kuwait_year = kuwait.drop('Month', axis = 1).groupby('Year').mean('AvgTemperature')
 kuwait['Date'] = datetime_month(kuwait)
 kuwait = drop_month_year(kuwait)
 
 dubai = df_create(dubai, ['Region', 'Country', 'City', 'Day'], ['Year', 'Month'])
+dubai_year = dubai.drop('Month', axis = 1).groupby('Year').mean('AvgTemperature')
 dubai['Date'] = datetime_month(dubai)
 dubai = drop_month_year(dubai)
 
 doha = df_create(doha, ['Region', 'Country', 'City', 'Day'], ['Year', 'Month'])
+doha_year = doha.drop('Month', axis = 1).groupby('Year').mean('AvgTemperature')
 doha['Date'] = datetime_month(doha)
 doha = drop_month_year(doha)
 
 chennai = df_create(chennai, ['Region', 'Country', 'City', 'Day'], ['Year', 'Month'])
+chennai_year = chennai.drop('Month', axis = 1).groupby('Year').mean('AvgTemperature')
 chennai['Date'] = datetime_month(chennai)
 chennai = drop_month_year(chennai)
 
 
 ## ARIMA PORTION
-result = adfuller(niamey)
-df_log = np.log(niamey)
-df_log_ma = df_log.rolling(2).mean()
-df_detrend = df_log - df_log_ma
-df_detrend.dropna(inplace=True)
 
-result = adfuller(df_detrend)
 
-df_log_diff = df_log.diff(periods=2).dropna()
-df_diff_rolling = df_log_diff.rolling(12)
-df_diff_std = df_diff_rolling.std()
-
-result = adfuller(df_log_diff)
 
 ## Using AutoArima but it looks like a diff of 2 periods with a log of 1 will be best
-# stepwise_fit = auto_arima(niamey['AvgTemperature'], start_p = 1, start_q = 1, max_p = 5, max_q = 5, m = 12,
+# stepwise_fit = auto_arima(kuwait['AvgTemperature'], start_p = 1, start_q = 1, max_p = 5, max_q = 5, m = 4,
 #                           start_P = 0, seasonal = True, d = None, D=1, trace = True, error_action = 'ignore',
 #                           suppress_warning = True, stepwise = True)
 
-from statsmodels.tsa.statespace.sarimax import SARIMAX 
 
-train = niamey.iloc[:len(niamey)- 24]
-test = niamey.iloc[len(niamey) - 24:]
+
+train = dubai.iloc[:len(dubai)- 24]
+test = dubai.iloc[len(dubai) - 24:]
 
 model = SARIMAX(train,
-                order = (1, 0 ,3),
-                seasonal_order =(4, 2, 1, 12) )
+                order = (1, 0 ,0),
+                seasonal_order =(2, 0, 0, 4) )
 result = model.fit()
 
+model = SARIMAX(dubai,  
+                    order = (1, 0, 0),  
+                    seasonal_order =(2, 1, 1, 12)) 
+result = model.fit()
+
+forecast = result.predict(start = len(dubai)  ,
+                          end = (len(dubai)) + 4,
+                          typ = 'levels').rename('Forecast')
 
 if __name__ == '__main__':
-    
-    ## Creating seasonal dceomposition graph
+    ## Line graph for Seasonal decomp Global
+    # Creating seasonal dceomposition graph
     # fig, axs = plt.subplots(4, figsize = (14, 12))
     # plot_seasonal_decomposition(axs, global_temp, seasonal_result)
     # fig.tight_layout()
@@ -148,19 +157,43 @@ if __name__ == '__main__':
     
     # city_highest.head(1).plot.bar(color = 'b',figsize =(14,8))
     # plt.show()
-    # print(niamey.head(50))
-    # print(niamey.head())
-    # print(kuwait.head())
-    # print(dubai.head())
-    # print(doha.head())
-    fig, ax = plt.subplots(figsize=(12, 8))
-
+    # fig, ax = plt.subplots(figsize=(12, 8))
+    fig, ax = plt.subplots(figsize=(20, 4))
     start = len(train)
     end = len(train) + len(test) - 1
 
     predictions = result.predict(start, end, typ='levels').rename('Predictions')
 
+    # Line graph of predictions and tests 
     ax.plot(predictions, label= 'prediction')
     ax.plot(test, label='Actual')
     ax.legend()
+
+
+    ## Line graph of top 5 hottest cities
+    # ax.plot(chennai_year, label = 'India, Chennai')
+    # ax.plot(doha_year, label = 'Qatar, Doha')
+    # ax.plot(kuwait_year, label = 'Kuwait, Kuwait')
+    # ax.plot(niamey_year, label = 'Nigeria, Niamey')
+    # ax.plot(dubai_year, label = 'UAE, Dubai')
+    # ax.legend()
+
+  
+
+    # fig, ax = plt.subplots(figsize=(20, 4))
+    # ax.plot(dubai.iloc[len(niamey)-24:], label = 'Avg Temp', color = 'b')
+    # ax.plot(forecast, label ='Forecast', color ='y', linewidth=2)
+    # ax.legend()
+
+    
+    ax.plot(dubai, label = 'Avg Temp', color = 'b')
+    ax.plot(forecast, label ='Forecast', color ='y', linewidth=2)
+    ax.legend()
+    
+    
+    
+
+    ##Line graph for Global temps rising
+    # fig, ax = plt.subplots(figsize=(12, 8))
+    # ax.plot(global_temp_year)
     plt.show()
